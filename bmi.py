612dfe5134ad95296d3893530e28cbbb1c207df9
Petillion Joris <joris.petillion@onderwijs.gent.be>
2026-02-04 11:25:08 +0100

    BMI Calculator

--- bmi.py
+++ bmi.py
@@ -9,7 +9,7 @@ s.write("Welkom, bereken hier je BMI")
 
 # Sliders
 g = s.slider("Hoeveel weeg je (in kg)?", 1, 120)
-l_cm = s.slider("Hoe groot ben je (in cm)?", 20, 230)
+l_cm = s.slider("Hoe groot ben je (in m)?", 0, 3)
 
 # Omrekenen naar meters
 l = l_cm / 100
