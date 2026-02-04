1e41b35601aa0af4c55c5c654b88816623ed36d5
Petillion Joris <joris.petillion@onderwijs.gent.be>
2026-02-04 11:26:52 +0100

    BMI Calculator

--- bmi.py
+++ bmi.py
@@ -1,21 +1,12 @@
 import streamlit as s
 import math as m
 
-# Titel
-s.title("Mijn Eerste Streamlit App")
+s.title("BMI calculator")
 
-# Tekst
-s.write("Welkom, bereken hier je BMI")
+g = s.slider("Gewicht (kg)", 1, 120)
+l = s.slider("Lengte (m)", 0.5, 2.5)
 
-# Sliders
-g = s.slider("Hoeveel weeg je (in kg)?", 1, 120)
-l_cm = s.slider("Hoe groot ben je (in m)?", 0, 3)
-
-# Omrekenen naar meters
-l = l_cm / 100
-
-# Berekening
 bmi = g / (l ** 2)
 
-# Output
-s.write(f"De BMI bij {g} kg en een lengte van {l_cm} cm is **{bmi:.2f}**")
+s.write(bmi)
+
